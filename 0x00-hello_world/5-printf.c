c9bb32e4e5472bf4acfff79712061eec6d5bcb95
Opeyemi <mustopha.yusufope@gmail.com>
2023-08-10 09:32:41 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Starting point of the program
+ * Return: 0 Always (successful)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
