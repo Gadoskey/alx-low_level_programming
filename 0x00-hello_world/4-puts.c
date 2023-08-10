cc1368d989f571c2d7a7363d806aedf68c863cfe
Opeyemi <mustopha.yusufope@gmail.com>
2023-08-10 09:21:21 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main - contains the strating point
- *return - the value returns 0 always
-*/
+ * main - Entry point of the program
+ * Return: 0 (success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
