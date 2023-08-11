c0c1f1af2406cc0af784fd76cee89d6183848834
Opeyemi <mustopha.yusufope@gmail.com>
2023-08-11 09:50:05 -0700

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - The starting point of the program
+ * Return: 0 Always
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	return (0);
+}
