fc00197ce019b731cf3bed454e8b754ac2516c5d
Opeyemi <mustopha.yusufope@gmail.com>
2023-08-12 05:05:15 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,8 +10,15 @@ int main(void)
 	for (i = 48 ; i <= 57 ; i++)
 	{
 		putchar(i);
+		if (i == 57)
+		{
+			continue;
+		}
 		putchar(',');
 		putchar(' ');
+	}
+	{
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
