23732a41df6dc1dac90db09961a3470a4bd02a05
Opeyemi <mustopha.yusufope@gmail.com>
2023-08-15 09:29:56 -0700

    1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet - A propgram that prints alphabet
+ * Return: Void
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a' ; c <= 'z' ; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
