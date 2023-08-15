b4bc1727f6f43568133c0b22fa39e6c36e6e4e2f
Opeyemi <mustopha.yusufope@gmail.com>
2023-08-15 15:31:24 -0700

    10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - A propgram that adds
+ * @n1: First value
+ * @n2: Second value
+ * Return: integer value
+ */
+int add(int n1, int n2)
+{
+
+	return (n1 + n2);
+
+}
