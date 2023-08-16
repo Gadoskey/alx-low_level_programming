47cb245aa674718e6881847e6f19c78a1b3edd29
Opeyemi <mustopha.yusufope@gmail.com>
2023-08-15 17:05:34 -0700

    5

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,39 +5,17 @@
  *print_to_98 - A propgram that prints to 98
  *@n:The integer
  *Return: 0
- */ 
+ */
 void print_to_98(int n)
 {
-	if (n <= 98)
-	{
-		for (; n <= 98; n++)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
-		}
-	}
+	int d;
+
+	if (n > 98)
+		for (d = n; d > 98; d--)
+			printf("%d, ", d);
+
 	else
-	{
-		for (; n >= 98; n--)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d ", n);
-			}
-		}
-	}
+		for (d = n; d < 98; d++)
+			printf("%d, ", d);
+	printf("98\n");
 }
