6703c6b36f4243e5122a75638c52085f9b7581d8
Opeyemi <mustopha.yusufope@gmail.com>
2023-08-16 04:56:02 -0700

    12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *print_times_table - A propgram that prints to 98
+ *@n:The integer
+ *Return: 0
+ */
+void print_times_table(int n)
+{
+	int d, m, re;
+
+	if (n <= 15 && n >= 0)
+		for (d = 0; d <= n; d++)
+		{
+			_putchar('0');
+			for (m = 1; m <= n; m++)
+			{
+				_putchar(',');
+				_putchar(' ');
+				re = d * m;
+				if (re <= 99)
+					_putchar(' ');
+				if (re <= 9)
+					_putchar(' ');
+				if (re >= 100)
+				{
+					_putchar((re / 100) + '0');
+					_putchar(((re / 10) % 10) + '0');
+				}
+				else if (re <= 99 && re >= 10)
+				{
+					_putchar((re / 10) + '0');
+				}
+				_putchar('\n');
+			}
+		}
+}
