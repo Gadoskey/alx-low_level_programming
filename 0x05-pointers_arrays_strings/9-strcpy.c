96ca7dbfd6203ea40788d7a68e0c6d394bf86b0f
Opeyemi <mustopha.yusufope@gmail.com>
2023-08-21 12:29:36 -0700

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strcpy - check the code
+ * @dest: The first pointer
+ * @src: The second pointer
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *dest_start = dest;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return dest_start;
+}
