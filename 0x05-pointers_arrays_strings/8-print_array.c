3bbbd0dd2208eab6e930b9b5ec3eb308cb3edb8a
Opeyemi <mustopha.yusufope@gmail.com>
2023-08-21 12:16:38 -0700

    8

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_array - check the code for
+ * @a: The first pointer
+ * @n: The second pointer
+ * Return: void
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+		{
+			printf(", ");
+		}
+	}
+	printf("\n");
+}
