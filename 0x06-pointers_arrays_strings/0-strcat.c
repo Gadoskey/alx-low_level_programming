ca1ed2e1d610c1a5a85d929eb8431322308c4b78
Opeyemi <mustopha.yusufope@gmail.com>
2023-08-23 10:02:54 -0700

    5

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _strcat - copies a string into another string
+ * @dest: The first pointer
+ * @src: The second pointer
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ptr = dest;
+
+	while (*ptr != '\0')
+	{
+		ptr++;
+	}
+	while (*src != '\0')
+	{
+		*ptr = *src;
+		ptr++;
+		src++;
+	}
+	*ptr = '\0';
+	return (dest);
+
+
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains more about pointers functions and arrays
