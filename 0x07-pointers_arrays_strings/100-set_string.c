88a6712ab370bfe7383742eb47436a3a9a129bd3
Opeyemi <mustopha.yusufope@gmail.com>
2023-08-28 06:07:38 -0700

    8

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string - check the code
+ * @s: double pointer
+ * @to: pointer
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <string.h>
 #include <stdbool.h>
 
