094d4505e07f5ed9c7d367cfeb8571d0b5874d31
Opeyemi <mustopha.yusufope@gmail.com>
2023-09-01 08:12:38 -0700

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -9,13 +9,13 @@
 
 int main(int argc, char *argv[])
 {
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int result = num1 * num2;
+	int num1 = 0, num2 = 0;
 
 	if (argc == 3)
 	{
-		printf("%d\n", result);
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		printf("%d\n", num1 * num2);
 	}
 	else
 	{
