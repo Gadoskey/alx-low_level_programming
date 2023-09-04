f330adac245f798c4213bc513db5ae0898421a89
Opeyemi <mustopha.yusufope@gmail.com>
2023-09-04 06:46:46 -0700

    3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,41 @@
+#include "main.h"
+/**
+ * alloc_grid - prints an array of characters
+ * @width: the character to print
+ * @height: the size
+ * Return: a pointer to char
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid, i, j;
+
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
+
+	grid = (int **)malloc(height * sizeof(int *));
+
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+		for (j = 0; j < height; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
