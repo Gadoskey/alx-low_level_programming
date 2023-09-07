6b71d18c541218352a77d5fd0cffb5d31c48a5a7
Opeyemi <mustopha.yusufope@gmail.com>
2023-09-07 05:18:26 -0700

    2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _calloc - prints buffer in hexa
+ * @nmemb: the address of memory to print
+ * @size: the size of the memory to print
+ * Return: Nothing.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *array;
+	unsigned int total;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+	total = nmemb * size;
+	array = malloc(total);
+	if (array == NULL)
+	{
+		return (NULL);
+	}
+	return (array);
+}
