0a29cf1c387be7257a81bbc84055199bb4013267
Opeyemi <mustopha.yusufope@gmail.com>
2023-09-11 09:59:17 -0700

    0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Defines a new struct about a dog
+ * @name: The name of the dog.
+ * @owner: The breed of the dog.
+ * @age: The age of the dog.
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+}
+#endif
