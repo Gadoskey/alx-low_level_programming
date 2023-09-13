741feaa74b2827b873717ffff44f3a77bc9a8ec9
Opeyemi <mustopha.yusufope@gmail.com>
2023-09-13 07:44:23 -0700

    2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+#include <stddef.h>
+#include "function_pointers.h"
+/**
+ * int_index - Prints an array of numbers
+ * @array: array
+ * @size: size of array
+ * @cmp: pointer function
+ * Return: 0
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+	if (array != NULL && cmp != NULL && size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
+}
+
+#endif
