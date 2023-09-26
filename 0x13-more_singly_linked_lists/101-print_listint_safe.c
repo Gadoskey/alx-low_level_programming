1ca40a2773dfc500dd4d34a4117668b027932906
Opeyemi <mustopha.yusufope@gmail.com>
2023-09-26 03:50:22 -0700

    new

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,27 +7,36 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *current = head;
-	size_t count = 0;
-	unsigned long int addresses[1024];
+	const listint_t *slow = head, *fast = head;
+	size_t count = 0, loop_detected = 0;
 
-	while (current != NULL)
+	while (fast && fast->next)
 	{
-		size_t i;
-
-		for (i = 0; i < count; i++)
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
 		{
-			if (addresses[i] == (unsigned long int)current)
-			{
-				printf("-> [%p] %d\n", (void *)current, current->n);
-				exit(98);
-			}
+			loop_detected = 1;
+			break;
 		}
-		addresses[count] = (unsigned long int)current;
+	}
+	if (loop_detected)
+	{
+		while (head != slow)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+			count++;
+		}
+		printf("[%p] %d\n", (void *)head, head->n);
+		printf("-> [%p] %d\n", (void *)head->next, head->next->n);
+		return (count + 2);
+	}
+	while (head)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		head = head->next;
 		count++;
-
-		printf("[%p] %d\n", (void *)current, current->n);
-		current = current->next;
 	}
 	return (count);
 }
