e6cfa23bd15c3083650d18f8067d323070dc7d67
Opeyemi <mustopha.yusufope@gmail.com>
2023-09-28 06:45:05 -0700

    5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -11,10 +11,10 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int xor_result = n ^ m;
 	unsigned int count = 0;
 
-	 while (xor_result)
-	 {
-		 count += xor_result & 1;
-		 xor_result >>= 1;
-	 }
-	 return (count);
+	while (xor_result)
+	{
+		count += xor_result & 1;
+		xor_result >>= 1;
+	}
+	return (count);
 }
