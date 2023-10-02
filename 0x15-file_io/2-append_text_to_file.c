35dc6201e16cd1476c025c4d9d09da2b5beca214
Opeyemi <mustopha.yusufope@gmail.com>
2023-10-02 06:36:13 -0700

    2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
-	fopen(filename, "a");
+	file = fopen(filename, "a");
 	if (file == NULL)
 	{
 		return (-1);
