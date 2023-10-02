df62cacec20ae60578e3e1559146a766ebeec5c3
Opeyemi <mustopha.yusufope@gmail.com>
2023-10-02 08:13:10 -0700

    3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -7,10 +7,12 @@
  * exit_with_message - exit program
  * @code: The code
  * @message: The message
+ * @param: parameter
  */
-void exit_with_message(int code, const char *message)
+void exit_with_message(int code, const char *message, const char *param)
 {
-	dprintf(2, "%s\n", message);
+	dprintf(2, message, param);
+	dprintf(2, "\n");
 	exit(code);
 }
 /**
@@ -27,19 +29,16 @@ int main(int argc, char *argv[])
 	char buffer[BUF_SIZE];
 
 	if (argc != 3)
-	{
-		exit_with_message(97, "Usage: cp file_from file_to");
-	}
+		exit_with_message(97, "Usage: cp file_from file_to", NULL);
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
-	{
-		exit_with_message(98, "Error: Can't read from file NAME_OF_THE_FILE");
-	}
+		exit_with_message(98, "Error: Can't read
+				from file NAME_OF_THE_FILE", argv[1]);
 	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
 		close(fd_from);
-		exit_with_message(99, "Error: Can't write to file NAME_OF_THE_FILE");
+		exit_with_message(99, "Error: Can't write to file NAME_OF_THE_FILE" argv[2]);
 	}
 	while ((bytes_read = read(fd_from, buffer, BUF_SIZE)) > 0)
 	{
@@ -48,14 +47,18 @@ int main(int argc, char *argv[])
 		{
 			close(fd_from);
 			close(fd_to);
-			exit_with_message(99, "Error: Can't write to file NAME_OF_THE_FILE");
+			exit_with_message(99, "Error: Can't write to
+					file NAME_OF_THE_FILE", argv[2]);
 		}
 	}
 	if (bytes_read == -1)
 	{
 		close(fd_from);
 		close(fd_to);
-		exit_with_message(98, "Error: Can't read from file NAME_OF_THE_FILE");
+		exit_with_message(98, "Error: Can't read from
+				file NAME_OF_THE_FILE", argv[1]);
 	}
+	if (close(fd_from) == -1 || close(fd_to) == -1)
+		exit_with_message(100, "Error: Can't close fd %d", fd_to);
 	return (0);
 }
