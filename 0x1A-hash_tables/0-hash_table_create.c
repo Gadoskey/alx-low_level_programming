4936940c62acd494c58740c41bf86bcf9db74e19
Opeyemi <mustopha.yusufope@gmail.com>
2024-02-09 00:15:29 -0800

    hash-tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,3 +1,4 @@
+/**Author -- Gadoskey**/
 #include "hash_tables.h"
 /**
  * hash_table_create - Creates a hash table.
@@ -17,7 +18,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (hash_table == NULL)
 		return (NULL);
 
-	hash_table->size = size;
 	hash_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (hash_table->array == NULL)
 	{
