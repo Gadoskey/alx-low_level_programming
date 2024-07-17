d4802bc144513430bd2c0c7ff416c4610f9ee4ba
Gadoskey <gadoski321@gmail.com>
2024-07-17 02:51:16 -0700

    new

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value)
+{
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	int i;
+	for (i = 0; i < size; i++)
+	{
+					        if (array[i] == value)
+							        {
+									            return (i);
+										            }
+						        
+						    }
+		        return (-1);
+}
