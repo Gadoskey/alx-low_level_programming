53353d173cd2f3ec3b26a161a433fced8f831979
Gadoskey <gadoski321@gmail.com>
2024-07-17 11:09:37 +0100

    u don start

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "search_algos.h"
 
 int linear_search(int *array, size_t size, int value)
 {
@@ -8,7 +9,7 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	}
 	int i;
-	
+
 	for (i = 0; i < size; i++)
 	{
 		if (array[i] == value)
